76dcfdcfecffe6386f1402810009359105a5b0cb
hanshu86 <myinbox0128@gmail.com>
2019-06-27 17:47:35 -0700

    perspective matrix and image warping

--- src/applyPerspectiveTransform.py
+++ src/applyPerspectiveTransform.py
@@ -3,21 +3,42 @@ import cv2
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 
-#testimage = mpimg.imread('../examples/warped_straight_lines.jpg')
-#print(testimage.shape)
+# testimage = mpimg.imread('../examples/warped_straight_lines.jpg')
+# plt.imshow(testimage)
+# plt.show()
+# exit()
+# #print(testimage.shape)
 # source image points
-srcPoints = np.float32([  	[167, 600],  # left bottom
-                    		[494 , 374], # left top
-                    		[572, 374], # right top
-                    		[931,  600] ]) # right bottom
 
-dstPoints = np.float32([  	[257, 600],  # left bottom
-                    		[257 , 0], # left top
-                    		[786, 0], # right top
-                    		[786,  600] ]) # right bottom
+def createSavePerspectiveMatrix(img):
+	# srcPoints = np.float32([  	[167, 600],  # left bottom
+	#                     		[494 , 374], # left top
+	#                     		[572, 374], # right top
+	#                     		[931,  600] ]) # right bottom
 
+	# dstPoints = np.float32([  	[257, 600],  # left bottom
+	#                     		[257 , 0], # left top
+	#                     		[786, 0], # right top
+	#                     		[786,  600] ]) # right bottom
 
-persPectiveMatrix = cv2.getPerspectiveTransform(src, dst)
+	srcPoints = np.float32([ [256, 720], [610 , 430], [700, 430], [1100,  720] ])
 
-img_size = (gray.shape[1], gray.shape[0])
-warped = cv2.warpPerspective(undist, persPectiveMatrix, img_size, flags=cv2.INTER_LINEAR)
+	offset = 100
+	img_size = (img.shape[1], img.shape[0])
+	dstPoints = np.float32([ [offset, 720], [offset, 0], [ 300, 0], [img_size[1] - offset, 720]])
+
+	persPectiveMatrix = cv2.getPerspectiveTransform(srcPoints, dstPoints)
+	np.save('persPectiveMatrix.npy', persPectiveMatrix)
+	return persPectiveMatrix
+
+def getWarpedImage(img):
+	try:
+		warpedMatrix = np.load('persPectiveMatrix')
+	except:
+		print("Perspective Matrix Does not exists..create one")
+		warpedMatrix = createSavePerspectiveMatrix(img)
+
+	img_size = (img.shape[1], img.shape[0])
+	print(img_size)
+	warped = cv2.warpPerspective(img, warpedMatrix, img_size, flags=cv2.INTER_LINEAR)
+	return warped
--- src/main.py
+++ src/main.py
@@ -8,9 +8,12 @@ import glob
 from undistort import undistortImage
 from thresholdBinaryImage import *
 from showImageSideBySide import *
+from applyPerspectiveTransform import *
 
-
-testimage = mpimg.imread('../test_images/test1.jpg')
+testimage = mpimg.imread('../test_images/straight_lines2.jpg')
 correctedImage = undistortImage(testimage)
-binary_image = getThresholdBinaryImage(correctedImage)
-showImageForComparison(testimage, binary_image, "Original Image", "Binary Image", gray_new_img=True)
+# showImageForComparison(testimage, correctedImage, "Original Image", "undistort Image")
+warped = getWarpedImage(correctedImage)
+showImageForComparison(testimage, warped, "Original Image", "Warped Image")
+#binary_image = getThresholdBinaryImage(correctedImage)
+#showImageForComparison(testimage, binary_image, "Original Image", "Binary Image", gray_new_img=True)
